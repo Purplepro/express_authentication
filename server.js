9a0dfd22b93d2dcf9d80ac5e3415a4eed22f0021
Sebastian G <gayle054@gmail.com>
2021-05-13 11:27:35 -0700

    SESSIONS SERVER

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
 const app = express();
+const flash = require('connect-flash');
+const session = require('express-session');
+
+
+const SECRET_SESSION = process.env.SECRET_SESSION;
+
+
 
 app.set('view engine', 'ejs');
 
@@ -9,6 +16,13 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+
+app.use(session({
+  secret: SECRET_SESSION,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(flash());
 
 app.get('/', (req, res) => {
   res.render('index');
